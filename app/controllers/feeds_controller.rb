c6d17d040c07107c986242b389256f781823ebb2
Gerard Dawson <gerard.dawson1@gmail.com>
2014-08-26 17:41:04 -0400

    Add feeds/users association

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,7 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @feeds = Feed.all
@@ -9,14 +11,14 @@ class FeedsController < ApplicationController
   end
 
   def new
-    @feed = Feed.new
+    @feed = current_user.feeds.build
   end
 
   def edit
   end
 
   def create
-    @feed = Feed.new(feed_params)
+    @feed = current_user.feeds.build(feed_params)
     if @feed.save
         redirect_to @feed, notice: 'Feed was successfully created.' 
       else
@@ -44,6 +46,11 @@ class FeedsController < ApplicationController
     def set_feed
       @feed = Feed.find(params[:id])
     end
+
+    def correct_user
+      @feed = current_user.feeds.find_by(id: params[:id])
+      redirect_to feeds_path, notice: "Not authorized to edit this feed." if @feed.nil?
+    end  
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
